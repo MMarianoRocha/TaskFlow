9dea7f5fde454111927f1abfe62f61a0f480d402
Matheus Mariano Rocha <matheusmarianomtd@gmail.com>
2026-04-17 19:16:17 -0300

    feat: Modelos Task e User com suporte async estruturados

--- models/task.py
+++ models/task.py
@@ -1,13 +1,18 @@
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import backref, declarative_base, relationship
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from database.base import Base
+
 
 class Task(Base):
     __tablename__ = "tarefas"
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    description = Column(String)
+    description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     user_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
     user = relationship("User", back_populates="tasks")
--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from database.base import Base
 class User(Base):
     __tablename__ = "usuarios"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, unique=True, nullable=False)
+    name = Column(String, index=True, unique=False, nullable=False)
     password = Column(String, nullable=False)
 
     tasks = relationship("Task", back_populates="user")
